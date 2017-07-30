c4594ae2e7ce1bee56bec9299ee69e48525d8409
陆美琴 <binglingwy@163.com>
2017-07-30 17:10:16 +0800

    添加commander

--- bin/mallcopy.js
+++ bin/mallcopy.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+var program = require('commander');
 
-console.log('mallcopy');
+program
+  .version('0.1.0')
+  .option('-p, --path <name>', 'target path')
+  .parse(process.argv);
+
+console.log('mallcopy1');
+console.log(program.path);
